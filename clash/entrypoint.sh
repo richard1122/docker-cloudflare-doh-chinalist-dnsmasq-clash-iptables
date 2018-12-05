4e48849611613f7724edf48203a68f6e8419d306
richard <richard9372@gmail.com>
2018-12-05 11:22:19 +0800

    try fix dns

--- clash/entrypoint.sh
+++ clash/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-dnsIp = $(getent hosts cloudflared | awk '{ print $1 }')
-
+exit
 iptables -t nat -N CLASH
 iptables -t nat -F CLASH
 iptables -t nat -A CLASH -m owner --uid-owner 900 --gid-owner 900 -j RETURN
-iptables -t nat -A CLASH -p udp -d ${dnsIp} -j RETURN
-iptables -t nat -A CLASH -p udp --dport 53 -j DNAT --to ${dnsIp}:53
+iptables -t nat -A CLASH -p udp --dport 53 -j DNAT --to 127.0.0.1:5300
 iptables -t nat -A CLASH -p tcp -j REDIRECT --to-ports 7892
 iptables -t nat -A OUTPUT -j CLASH
 iptables -t nat -A PPREROUTINE -j CLASH
--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -e
 exec dnsmasq --conf-dir=/etc/dnsmasq.d/ \
     --cache-size=25000 \
     --server="$(getent hosts cloudflared | awk '{ print $1 }')#5300" \
